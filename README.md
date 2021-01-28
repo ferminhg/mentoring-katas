148da43c92f5827b9762c45327753705efc12409
fermin <fermin.hdez@gmail.com>
2021-01-28 17:17:30 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# Coding together
+
+## Problems
+
+### Hackerank
+
+[Birthday Cake Candles](hackerank/js-practice/birthday-cake-candles.js)
+
+Execute problem:
+```bash
+node hackerank/js-practice/birthday-cake-candles.js
+```
---
+++ hackerank/js-practice/birthday-cake-candles.js
@@ -0,0 +1,32 @@
+'use strict';
+// https://www.hackerrank.com/challenges/time-conversion/problem
+function birthdayCakeCandles(candles) {
+    let numMaxCandles = 1;
+    // ciclo for para buscar el numero maximo
+    // IDEA: probar una funcion que se llama max
+    let numeroMasAlto = -1
+    candles.forEach(element => {
+        if (element > numeroMasAlto)
+            numeroMasAlto = element;
+    });
+
+    const result = candles.filter(numCandles => numCandles==numeroMasAlto);
+    return result.length;
+}
+
+
+function execute(candles, exceptedResult) {
+    const result = birthdayCakeCandles(candles);
+    if (result == exceptedResult) {
+        console.log('👍');
+    } else {
+        console.log('👎');
+    }
+}
+
+execute([4, 4, 1, 3], 2);
+execute([3, 2, 1, 3], 2);
+execute([1, 2, 4, 5], 1);
+execute([1, 1], 2);
+execute([1, 1, 12,3,123, 12, 21,23], 1);
+execute([4,4,4,4,4,4,4,4,4,4,4,4,4], 13);
