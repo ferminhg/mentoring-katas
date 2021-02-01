9bcd63e9b9b1faca549c3ae1778ea2e0bdc2960c
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-02-01 18:21:31 +0100

    Add Math.max function to achieve the hightest number

--- hackerank/js-practice/birthday-cake-candles.js
+++ hackerank/js-practice/birthday-cake-candles.js
@@ -1,32 +1,25 @@
-'use strict';
+"use strict";
 // https://www.hackerrank.com/challenges/time-conversion/problem
 function birthdayCakeCandles(candles) {
-    let numMaxCandles = 1;
-    // ciclo for para buscar el numero maximo
-    // IDEA: probar una funcion que se llama max
-    let numeroMasAlto = -1
-    candles.forEach(element => {
-        if (element > numeroMasAlto)
-            numeroMasAlto = element;
-    });
+  // IDEA: probar una funcion que se llama max
+  const numeroMasAlto = Math.max(...candles);
 
-    const result = candles.filter(numCandles => numCandles==numeroMasAlto);
-    return result.length;
+  const result = candles.filter((numCandles) => numCandles == numeroMasAlto);
+  return result.length;
 }
 
-
 function execute(candles, exceptedResult) {
-    const result = birthdayCakeCandles(candles);
-    if (result == exceptedResult) {
-        console.log('👍');
-    } else {
-        console.log('👎');
-    }
+  const result = birthdayCakeCandles(candles);
+  if (result == exceptedResult) {
+    console.log("👍");
+  } else {
+    console.log("👎");
+  }
 }
 
 execute([4, 4, 1, 3], 2);
 execute([3, 2, 1, 3], 2);
 execute([1, 2, 4, 5], 1);
 execute([1, 1], 2);
-execute([1, 1, 12,3,123, 12, 21,23], 1);
-execute([4,4,4,4,4,4,4,4,4,4,4,4,4], 13);
+execute([1, 1, 12, 3, 123, 12, 21, 23], 1);
+execute([4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4], 13);
