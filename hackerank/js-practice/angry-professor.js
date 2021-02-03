a26c4f94c4a132ac10a2527968acb76c86e1ac1d
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-02-03 14:15:14 +0100

    Add responses into a variable, remove condition in else statement and add an example when the response is NO

--- hackerank/js-practice/angry-professor.js
+++ hackerank/js-practice/angry-professor.js
@@ -2,18 +2,23 @@
 
 // Complete the angryProfessor function below.
 function angryProfessor(studentsThreshold, arrivalTime) {
+  const canceledClass = "YES";
+  const notCanceledClass = "NO";
+
   const arrivedStudents = arrivalTime.filter((studentTime) => studentTime <= 0);
   if (arrivedStudents.length < studentsThreshold) {
-    return "YES";
+    return canceledClass;
   }
-  return "NO";
+  return notCanceledClass;
 }
 
 function main(studentsThreshold, arrivalTime, response) {
-
   let result = angryProfessor(studentsThreshold, arrivalTime);
   if (result === response) {
     console.log("ok");
-  } else if (result !== response) console.log("not ok");
+  } else {
+    console.log("not ok");
+  }
 }
-main(3, [-1, -2, 1, 3, 4], 'YES');
+main(3, [-1, -2, 1, 3, 4], "YES");
+main(4, [-1, -2, 0, -3, 4], "NO");
