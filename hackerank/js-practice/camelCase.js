0299aa8ffa2a28855c623e160c245489e355d1dc
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-02-04 18:08:52 +0100

    Changed variables' names and tab properly the code

--- hackerank/js-practice/camelCase.js
+++ hackerank/js-practice/camelCase.js
@@ -1,11 +1,11 @@
-function camelcase(s) {
-   let count = 1;
-   for(let i in s){
-    if(s.charAt(i) === s.charAt(i).toUpperCase()){
-      count++
-    }
-   }
-   return count
+function camelcaseString(s) {
+	let wordsNumber = 1;
+	for (let i in s) {
+		if (s.charAt(i) === s.charAt(i).toUpperCase()) {
+			wordsNumber++;
+		}
+	}
+	return wordsNumber;
 }
 
-console.log(camelcase("saveChangesInTheEditor"));
+console.log(camelcase('saveChangesInTheEditor'));
