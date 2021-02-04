8cc04481c053006ccee5c7a5818957ead3de6543
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-02-04 11:10:03 +0100

    added file camelCase.js

---
+++ hackerank/js-practice/camelCase.js
@@ -0,0 +1,11 @@
+function camelcase(s) {
+   let count = 1;
+   for(let i in s){
+    if(s.charAt(i) === s.charAt(i).toUpperCase()){
+      count++
+    }
+   }
+   return count
+}
+
+console.log(camelcase("saveChangesInTheEditor"));
