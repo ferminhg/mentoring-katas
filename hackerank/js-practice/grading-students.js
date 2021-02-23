3bddcf7a3c53a21dffe24f4ad1851e6bbd2659c9
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-02-23 11:53:11 +0100

    changed map

--- hackerank/js-practice/grading-students.js
+++ hackerank/js-practice/grading-students.js
@@ -1,5 +1,5 @@
 function gradingStudents(grades) {
-	return grades.map((grade) => {
+	return grades.map((studentGrade) => {
 		const round = Math.ceil((grade + 1) / 5) * 5;
 		if (studentGrade < 38) {
 			return studentGrade;
